e009fb1c191ccb4be223ea4d3e6d0145fb432cb8
soham-shah <sohamshah225@gmail.com>
2017-11-14 08:50:02 -0700

    updates
    added data cleaning for the script

--- README.md
+++ README.md
@@ -8,3 +8,6 @@
 
 ##Project
 Analyze the Kaggle homicide reports dataset
+
+##Questions:
+- Is there a correlation between the relationships between the victim and the perpetrator?
--- scripts/basic.py
+++ scripts/basic.py
@@ -1,4 +1,19 @@
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+# answer: Is there a correlation between the relationships between the victim and the perpetrator?
 
 dataArr = pd.read_csv("../data/database.csv")
-print(dataArr.head(n=2))
+dataArr = (dataArr.drop(['Record ID', 'Agency Code','Agency Name','Agency Type','City', 'State', 'Year','Month', 'Incident', 'Crime Type', 'Crime Solved'],axis=1))
+# print(dataArr.head(n=1))
+dataArr = dataArr[dataArr["Relationship"] != "Unknown"]
+grouped = dataArr.groupby("Relationship").size().reset_index()
+# print(grouped.columns.values.tolist())
+grouped = grouped.sort_values(0, ascending=False)
+
+print(grouped)
+grouped.plot(kind="bar")
+# plt.bar(grouped[0],grouped[1])
+# print(grouped[grouped.columns[0]])
+plt.show()
