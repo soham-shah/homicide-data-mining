58bdc2a30c01bc3d9bb498637a241b0bd0fb2754
soham-shah <sohamshah225@gmail.com>
2017-11-14 10:10:14 -0700

    notes
    added comments on what each function does.

--- scripts/basic.py
+++ scripts/basic.py
@@ -2,18 +2,22 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# answer: Is there a correlation between the relationships between the victim and the perpetrator?
+# Question: Is there a correlation between the relationships between the victim and the perpetrator?
 
 dataArr = pd.read_csv("../data/database.csv")
+
+# remove these columns
 dataArr = (dataArr.drop(['Record ID', 'Agency Code','Agency Name','Agency Type','City', 'State', 'Year','Month', 'Incident', 'Crime Type', 'Crime Solved'],axis=1))
 # print(dataArr.head(n=1))
+
+# remove rows where the relationship is unknown
 dataArr = dataArr[dataArr["Relationship"] != "Unknown"]
+
+# get count of each uniqie thing in Relationship and sort
 grouped = dataArr.groupby("Relationship").size().reset_index()
-# print(grouped.columns.values.tolist())
 grouped = grouped.sort_values(0, ascending=False)
 
+# plot the result
 print(grouped)
 grouped.plot(kind="bar")
-# plt.bar(grouped[0],grouped[1])
-# print(grouped[grouped.columns[0]])
 plt.show()
