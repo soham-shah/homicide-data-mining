d9a0dfce9c1d3328f94a4f55540335f77111e666
nhng5827 <contactus@sendacrate.com>
2017-11-14 19:49:03 -0700

    initial plot question 3

--- scripts/q3_basic.py
+++ scripts/q3_basic.py
@@ -13,6 +13,36 @@ dataArr = (dataArr.drop(['Record ID', 'Agency Code','Agency Name','Agency Type',
 
 dataArr["Victim Count"] = dataArr["Victim Count"]+1
 dataArr["Perpetrator Count"] = dataArr["Perpetrator Count"]+1
-#print(dataArr.head(n=10))
+num_vic = dataArr["Victim Count"].value_counts()
+num_perp = dataArr["Perpetrator Count"].value_counts()
 
+x_pos = np.arange(len(num_vic))
+x_pos2 = np.arange(len(num_vic)-1)
+width = 0.25
+fig = plt.figure()
+ax = fig.add_subplot(121)
+ax2 = fig.add_subplot(122)
 
+rects1 = ax.bar(x_pos, num_vic[:], width, color='r')
+rects2 = ax.bar(x_pos+width, num_perp[:], width, color='g')
+rects12 = ax2.bar(x_pos2, num_vic[1:], width, color='r')
+rects22 = ax2.bar(x_pos2+width, num_perp[1:], width, color='g')
+
+ax.set_ylabel('Bin Size')
+ax.set_xlabel('Number of Victims/Perpetrators')
+ax.set_xticks(x_pos+width)
+ax.set_xticklabels(('1','2','3','4','5','6','7','8','9','10','11'))
+ax.legend((rects1[0], rects2[0]), ('Victims', 'Perpetrators'))
+
+ax2.set_ylabel('Bin Size')
+ax2.set_xlabel('Number of Victims/Perpetrators')
+ax2.set_xticks(x_pos2+width)
+ax2.set_xticklabels(('2','3','4','5','6','7','8','9','10','11'))
+ax2.legend((rects12[0], rects22[0]), ('Victims', 'Perpetrators'))
+
+# fix the viewport to grab everything
+plt.tight_layout()
+# Note, save your output to the plots folder. name it something
+plt.savefig('../plots/vic_perp_count_1.png')
+
+plt.show()
