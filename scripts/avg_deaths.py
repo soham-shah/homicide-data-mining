828bd69f8c5de16ce2b72d8bfc22cc17e5f1f41e
gramkuma <gira3678@colorado.edu>
2017-12-13 03:31:37 -0700

    Another script and plot
    Added most homicides committed per state analysis.

--- scripts/avg_deaths.py
+++ scripts/avg_deaths.py
@@ -10,16 +10,10 @@ dataArr = pd.read_csv("../data/database.csv")
 
 # remove these columns
 dataArr = (dataArr.drop(['Record ID', 'Agency Code','Agency Name','Agency Type','City', 'Weapon', 'Incident', 'Crime Type', 'Crime Solved', 'Perpetrator Count', 'Perpetrator Race', 'Victim Race', 'Perpetrator Age', 'Victim Ethnicity', 'Perpetrator Ethnicity', 'Relationship','Record Source', 'Victim Age', 'Victim Sex', 'Perpetrator Sex'],axis=1))
-#print(dataArr.head(n=10))
-
-#year = dataArr.loc[dataArr['Year'] == 1980]
-#state = dataArr.loc[dataArr['State'] == "Alaska"]
 
 # df['column_name'].isin(some_values)
 
 year = 1980,1981,1982,1983,1984,1985,9186,1987,1988,1989,1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014
-#state = "Colorado"
-#month = "August"
 
 # Make a data frame that is populated with the variables we want
 
